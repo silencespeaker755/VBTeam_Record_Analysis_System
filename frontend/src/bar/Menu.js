464dac3fcd5d27088f3fd2f0168cd32120392b3b
silencespeaker <b06902019@ntu.edu.tw>
2021-06-22 05:18:35 +0800

    Add basic right drawer with some equipment

--- frontend/src/bar/Menu.js
+++ frontend/src/bar/Menu.js
@@ -1,29 +1,88 @@
 import React, { useState } from "react";
-import { makeStyles, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Badge, ClickAwayListener, IconButton } from "@material-ui/core";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+import RightDrawer from "./RightDrawer";
 
 const useStyles = makeStyles((theme) => ({
   section: {
     display: "none",
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up("sm")]: {
       display: "flex",
     },
+  },
+  flex: {
+    display: "flex",
   },
 }));
 
 export default function Menu() {
   const classes = useStyles();
+
+  const [type, setType] = useState("");
+  const [open, setOpen] = useState(false);
+
+  // data
+  const username = "Jason";
+
+  const toggleDrawer = (bool) => (e) => {
+    setOpen(bool);
+  };
+
+  const handleClickAway = () => {
+    setType("");
+  };
+
+  if (!username)
+    return (
+      <div className={classes.section}>
+        <IconButton
+          edge="end"
+          onClick={() => {}}
+          color="inherit"
+          component="span"
+        >
+          <AccountCircleIcon />
+        </IconButton>
+        {/* Drawer */}
+      </div>
+    );
   return (
     <div className={classes.section}>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <div className={classes.flex}>
+          <IconButton onClick={() => setType("mails")} component="span">
+            <Badge badgeContent={0} color="secondary">
+              <MailIcon
+                style={{
+                  color: type === "mails" ? "#5ace5a" : "white",
+                }}
+              />
+            </Badge>
+          </IconButton>
+          <IconButton onClick={() => setType("announcements")} component="span">
+            <Badge badgeContent={1} color="secondary">
+              <NotificationsIcon
+                style={{
+                  color: type === "announcements" ? "#5ace5a" : "white",
+                }}
+              />
+            </Badge>
+          </IconButton>
+        </div>
+      </ClickAwayListener>
       <IconButton
         edge="end"
-        onClick={() => {}}
+        onClick={toggleDrawer(true)}
         color="inherit"
         component="span"
       >
         <AccountCircleIcon />
       </IconButton>
-      {/* Drawer */}
+      <RightDrawer open={open} toggleDrawer={toggleDrawer} />
     </div>
   );
 }
---
+++ frontend/src/bar/RightDrawer.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
+import ListIcon from "@material-ui/icons/List";
+
+const useStyle = makeStyles((theme) => ({
+  flex: {
+    display: "flex",
+  },
+  list: {
+    position: "relative",
+    display: "flex",
+    overflow: "auto",
+    flexGrow: 1,
+  },
+  listItems: {
+    width: "250px",
+  },
+  listItem: {
+    "&:hover": {
+      backgroundColor: "#9e9e9e",
+      color: "white",
+      //   "& .MuiListItemIcon-root": {
+      //     color: "white",
+      //   },
+    },
+  },
+  notAdmin: {
+    display: "none",
+  },
+  width_100: {
+    width: "100%",
+  },
+  white: {
+    color: "white",
+  },
+  link: {
+    textDecoration: "none",
+  },
+}));
+
+export default function RightDrawer(props) {
+  const { open, toggleDrawer } = props;
+  const classes = useStyle();
+
+  const [admin, setAdmin] = useState(true);
+
+  const menuList = [
+    {
+      label: "My account",
+      icon: <AccountCircleIcon />,
+      link: `/home`,
+      event: null,
+      visible: admin,
+    },
+    {
+      label: "My Post",
+      icon: <ListIcon />,
+      link: `/home`,
+      event: null,
+      visible: admin,
+    },
+    {
+      label: "My Vedio",
+      icon: <ListIcon />,
+      link: `/home`,
+      event: null,
+      visible: admin,
+    },
+    {
+      label: "Broadcast",
+      icon: <OpenInNewIcon />,
+      link: "/",
+      event: null,
+      visible: admin,
+    },
+  ];
+
+  return (
+    <Drawer
+      anchor="right"
+      className={classes.flex}
+      PaperProps={{ style: { backgroundColor: "#757575" } }}
+      open={open}
+      onClose={toggleDrawer(false)}
+    >
+      <div className={classes.list} onClick={() => toggleDrawer(false)}>
+        <div className={classes.listItems}>
+          <List>
+            {menuList.map((item) => (
+              <Link
+                to={item.link}
+                key={item.label}
+                className={clsx(classes.link, {
+                  [classes.notAdmin]: !item.visible,
+                })}
+                onClick={item.event}
+              >
+                <ListItem className={classes.listItem} button>
+                  <ListItemIcon className={classes.white}>
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    className={classes.white}
+                    primary={item.label}
+                  />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </div>
+      </div>
+      <div className={classes.width_100} onClick={toggleDrawer(false)}>
+        <Link to="/" className={classes.link} onClick={() => {}}>
+          <ListItem className={classes.listItem} button>
+            <ListItemIcon className={classes.white}>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText className={classes.white} primary="log out" />
+          </ListItem>
+        </Link>
+      </div>
+    </Drawer>
+  );
+}
