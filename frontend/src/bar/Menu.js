f833032c55127209652a83167a66c57b09c063d0
silencespeaker <b06902019@ntu.edu.tw>
2021-06-21 14:03:48 +0800

    Add a rightest bar item to expend drawer

--- frontend/src/bar/Menu.js
+++ frontend/src/bar/Menu.js
@@ -1,5 +1,29 @@
 import React, { useState } from "react";
+import { makeStyles, IconButton } from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+}));
 
 export default function Menu() {
-  return null;
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <IconButton
+        edge="end"
+        onClick={() => {}}
+        color="inherit"
+        component="span"
+      >
+        <AccountCircleIcon />
+      </IconButton>
+      {/* Drawer */}
+    </div>
+  );
 }
--- frontend/src/homepage/ScrollTopButton.js
+++ frontend/src/homepage/ScrollTopButton.js
@@ -13,10 +13,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ScrollTop(props) {
   const { children, window } = props;
+
   const classes = useStyles();
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
     disableHysteresis: true,
@@ -27,8 +25,6 @@ export default function ScrollTop(props) {
     const anchor = (event.target.ownerDocument || document).querySelector(
       "#back-to-top-anchor"
     );
-
-    console.log(anchor);
 
     if (anchor) {
       anchor.scrollIntoView({ behavior: "smooth", block: "center" });
