e3a98fa2733f02a569aa47ef655b1c5e6547c022
silencespeaker <b06902019@ntu.edu.tw>
2021-06-22 12:56:00 +0800

    Add custom icon on calendar

--- frontend/src/homepage/Calendar.js
+++ frontend/src/homepage/Calendar.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction";
+import $, { data } from "jquery";
+import moment from "moment";
+import "../css/Calendar.css";
 
 export default function Calendar(props) {
   const { showErrorModel } = props;
+  const [edit, setEdit] = useState(false);
 
-  const handleDateClick = (arg) => {
-    showErrorModel();
+  const handleEdit = (event) => (e) => {
+    setEdit(true);
+  };
+
+  const handleEventRender = ({ event, el, view, timeText }) => {
+    const dateAttr = moment(event.start).format("YYYY-MM-DD");
+    if (!$(`[data-date='${dateAttr}']`).hasClass("detected")) {
+      $(`[data-date='${dateAttr}']`).append(
+        `
+        <span id='${dateAttr}'>
+          <img src='create_pen.svg' class='custom_edit_pen' alt='modify calendar' />
+        </span>
+        <span class='custom_attend_number'>5</span>
+        `
+      );
+      $(`#${dateAttr}`).on("click", handleEdit(event));
+      $(`[data-date='${dateAttr}']`).addClass("detected");
+    }
+    return el;
+  };
+
+  const handleDateClick = (e) => {
+    if (!edit) showErrorModel();
   };
 
   return (
@@ -15,11 +41,14 @@ export default function Calendar(props) {
       style={{
         margin: "40px",
         border: "20px solid #a78758",
-        borderRadius: "23px",
+        borderRadius: "25px",
       }}
     >
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
+        viewClassNames="control-modify-entry"
+        dayCellClassNames="test-day-class"
+        eventContent={handleEventRender}
         headerToolbar={{
           left: "prev,next",
           center: "title",
@@ -29,12 +58,16 @@ export default function Calendar(props) {
         events={[
           { title: "event 1", date: "2021-06-01" },
           { title: "event 2", date: "2021-06-02" },
-          { title: "練球", start: "2021-06-23T12:30:00", end: "2021-06-25" },
+          {
+            title: "練球",
+            start: "2021-06-23T12:30:00",
+            end: "2021-06-25",
+          },
+          { title: "練球", start: "2021-06-23T14:30:00", end: "2021-06-25" },
         ]}
         dateClick={handleDateClick}
         initialDate="2021-06-01"
         eventClick={() => {}}
-        eventRender={() => {}}
         editable
       />
     </div>
