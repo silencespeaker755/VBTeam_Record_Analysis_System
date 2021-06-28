ed91923f8559b637d3fd71a4ccfb0a999c1e8c2e
momo <nomofika@gmail.com>
2021-06-29 01:51:47 +0800

    split api for set create & delete

--- backend/routes/api/match/records/sets/index.js
+++ backend/routes/api/match/records/sets/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import dataRouter from "./data";
+import setRouter from "./sets";
 
 const router = Router();
 
+router.use("/", setRouter);
 router.use("/data", dataRouter);
 
 export default router;
---
+++ backend/routes/api/match/records/sets/sets.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import RecordService from "../../../../../services/RecordService";
+
+const router = Router();
+
+router.post("/create", (req, res) => {
+  console.log("/api/match/records/sets/create: ", req.body);
+
+  const { recordId, userId } = req.body;
+
+  RecordService.createSet({ recordId, userId })
+    .then((setId) => {
+      console.log("Created set:", setId);
+      /*
+        #swagger.responses[200] = { 
+          schema: {
+            id: "60d619a88da34eda2a6ebc41"
+          }
+        }
+      */
+      res.status(200).json({ id: setId });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err === "Admin required") {
+        res.status(403).send(err);
+      } else {
+        res.status(404).send(err);
+      }
+    });
+});
+
+router.post("/delete", (req, res) => {
+  console.log("/api/match/records/sets/delete: ", req.body);
+
+  const { recordId, setId, userId } = req.body;
+  RecordService.deleteSet({ recordId, setId, userId })
+    .then((set) => {
+      console.log("Deleted set:", set);
+      res.status(200).send("Delete success!");
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err === "Admin required!") {
+        res.status(403).send(err);
+      } else {
+        res.status(404).send(err);
+      }
+    });
+});
+
+export default router;
