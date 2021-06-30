235392dd21f02eaa4696a6244d20f057da9f07db
silencespeaker <b06902019@ntu.edu.tw>
2021-07-01 02:15:45 +0800

    Add heroku command

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import loaders from "./loaders";
 import config from "./config";
 
@@ -6,6 +7,14 @@ async function startServer() {
   const app = express();
 
   await loaders({ expressApp: app });
+
+  // const isProduction = process.env.NODE_ENV === "production";
+  // if (isProduction) {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+  // }
 
   app.listen(config.port || 5000, (err) => {
     if (err) {
