611c374485bb4edecaf7f7ccc4922abde558d76a
silencespeaker <b06902019@ntu.edu.tw>
2021-06-30 20:07:47 +0800

    Change frontend route

--- frontend/src/setting.js
+++ frontend/src/setting.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:5000" });
+const instance = axios.create({
+  baseURL: "https://ntucsievolleyballserver.herokuapp.com/",
+});
 
 export { instance as default };
